b87e6774fef561d2eb05d7acc28a204061187494
Mikhail <manylattice7742@yandex.kz>
2020-09-09 11:18:00 +0600

    Update Package

--- Package.swift
+++ Package.swift
@@ -6,23 +6,13 @@ import PackageDescription
 let package = Package(
     name: "FlexiblePageControl",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FlexiblePageControl",
             targets: ["FlexiblePageControl"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FlexiblePageControl",
             dependencies: []),
-        .testTarget(
-            name: "FlexiblePageControlTests",
-            dependencies: ["FlexiblePageControl"]),
     ]
 )
